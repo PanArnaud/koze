d93363abe118d6fdb6a77d47dcbed12d784d0c54
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2024-03-17 00:04:48 +0400

    Learn page wrapper

--- components/sidebar-item.tsx
+++ components/sidebar-item.tsx
@@ -3,8 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Button } from "./ui/button";
-
+import { Button } from "@/components/ui/button";
 type Props = {
   label: string;
   iconSrc: string;
