d7fce1860a606bf0cdfa8d704a1d471865213c54
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2024-03-18 23:38:28 +0400

    setup uinits

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,14 +1,18 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Header } from "./header";
 
 const LearnPage = async () => {
   const userProgressPromise = getUserProgress();
+  const unitsPromise = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressPromise]);
+  const [userProgress, units] = await Promise.all([
+    userProgressPromise,
+    unitsPromise,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
@@ -26,6 +30,11 @@ const LearnPage = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
